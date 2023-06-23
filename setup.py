92dba1d2426891a685c698325f8450542296dfdc
lagudu-glitch <laguduvishnu@gmail.com>
2023-06-23 10:36:17 -0700

    Add setup

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-import subprocess
+import subprocess, os
 
 def setup ():
-    subprocess.call ("mv OpenLane-flow/cvc_pdk .", shell=True)
+    os.chdir("../")
+    subprocess.call ("mv OpenLane-flow/cvc_pdk ../", shell=True)
     subprocess.call ("mv OpenLane-flow/makefile .", shell=True)
     subprocess.call ("mv OpenLane-flow/config.json .", shell=True)
     subprocess.call ("mv OpenLane-flow/time_sim.py .", shell=True)
     # Only exists for TA example
-    subprocess.call ("mv OpenLane-flow/src .", shell=True)
+    subprocess.call ("mv OpenLane-flow/src . -y", shell=True)
+    subprocess.call ("rm -rf OpenLane-flow", shell=True)
+
 
 if __name__ == "__main__":
     setup ()
